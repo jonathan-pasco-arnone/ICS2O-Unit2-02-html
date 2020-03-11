098682565ed7766e57d881902709be08ee484af4
Ubuntu <ubuntu@ip-172-31-29-227.ec2.internal>
2020-03-11 17:45:45 +0000

    added stuff

---
+++ file.js
@@ -0,0 +1,14 @@
+function change () {
+  var image = document.getElementById('helloworld');
+  image.src = "./images/World.jpg";}
+function coloring() {
+  var str = "Hello, World!";
+  document.getElementById("color").innerHTML = str;}
+function appearing() {
+  var str = "Hello";
+  document.getElementById("appear").innerHTML = str;}
+function disappearing() {
+  document.getElementById("appear").style.display="none";}
+function background() {
+  document.body.style.background = "yellow";
+}
